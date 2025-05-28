5a65da22e0aaebcb112bcca3fea1a04d5a9c5ae7
kenkenkankan <kenneth.joshuu@gmail.com>
2025-05-28 13:54:34 +0700

    project start

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,55 +1,99 @@
+using System.Collections;
+using System.Globalization;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    [SerializeField] private DialogueUI dialogueUI;
+    [SerializeField] private Transform flashlightAnchor;
+
+    public DialogueUI DialogueUI => dialogueUI;
+    public IInteractable Interactable { get; set; }
+
     public float moveSpeed;
-    public bool isMoving = false;
-    
-    Vector3 mousePos;
-    Vector3 mouseWorldPos;
-    Rigidbody rb;
+    public bool isMoving;
+    private Vector2 input;
+
+    public LayerMask solidObjectsLayer;
+    public LayerMask interactableLayer;
+
+    private Animator animator;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0)){
-            Movement();
-            isMoving = true;
-        }
-    }
+        if (dialogueUI.IsOpen) return;
 
-    void FixedUpdate()
-    {
-        if(isMoving)
-            Move(mouseWorldPos);
-    }
-
-    void Movement()
-    {
-        rb.velocity = Vector3.zero;
-
-        mousePos = Input.mousePosition;
-        mousePos.z = Camera.main.nearClipPlane;
-        mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
-    
-        mouseWorldPos.y = mouseWorldPos.z = 0;
-    }
-
-    void Move(Vector3 targetPos)
-    {
-        Vector3 newPosition = Vector3.MoveTowards(rb.position, targetPos, moveSpeed * Time.fixedDeltaTime);
-        rb.MovePosition(newPosition);
-
-        float compare = 2.7f;
-        // Berhenti ketika sudah dekat dengan target
-        // Value pembanding  tergantung size collider sepertinya
-        if (Vector3.Distance(newPosition, targetPos) <= compare)
+        if (!isMoving) 
         {
-            rb.velocity = Vector3.zero;
-            isMoving = false;
+            input.x = Input.GetAxisRaw("Horizontal");
+
+            if (input.x != 0)
+            {
+                Vector3 scale = flashlightAnchor.localScale;
+                scale.x = -Mathf.Sign(input.x); // flip arah X
+                flashlightAnchor.localScale = scale;
+            }
+
+            
+
+            if (input.x != 0) input.y = 0;
+
+            if (input != Vector2.zero)
+            {
+                animator.SetFloat("MoveX", input.x);
+                
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+
+                if (IsWalkable(targetPos))
+                StartCoroutine(Move(targetPos));
+            }
+        }
+
+        animator.SetBool("IsMoving", isMoving);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Interactable != null)
+            {
+                Interactable?.Interact(this);
+            }
         }
     }
+
+   IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+
+    private bool IsWalkable(Vector3 targetPos)
+    {
+        if(Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer | interactableLayer) != null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
 }
